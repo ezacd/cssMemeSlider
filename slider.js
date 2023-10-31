7369e7ca26f82d9785235c074a3cb3c880aa3b67
ezacd <119522254+ezacd@users.noreply.github.com>
2023-10-31 20:52:33 +0300

    task complete

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 const imgWidth = 1400;
-const textWidth = 1000;
+const textWidth = 1100;
 
 const img = img_slider.querySelector('ul');
 const text = text_slider.querySelector('ul');
@@ -7,25 +7,37 @@ const text = text_slider.querySelector('ul');
 const imgElems = img_slider.querySelector('li');
 const textElems = text_slider.querySelector('li');
 
-let pos = 0
+let imgPos = 0;
+let textPos = 0;
 
 document.getElementById('color1').onclick = function() {
-    pos = 0
-    list.style.marginLeft = pos
+    imgPos = 0
+    img.style.marginLeft = imgPos
+
+    textPos = 0
+    text.style.marginLeft = textPos
 }
 
 document.getElementById('color2').onclick = function() {
-    pos = -width
-    list.style.marginLeft = pos + 'px'
+    imgPos = -imgWidth
+    img.style.marginLeft = imgPos + 'px'
 
+    textPos = -textWidth
+    text.style.marginLeft = textPos + 'px'
 }
 
 document.getElementById('color3').onclick = function() {
-    pos = -width * 2
-    list.style.marginLeft = pos + 'px'
+    imgPos = -imgWidth * 2
+    img.style.marginLeft = imgPos + 'px'
+
+    textPos = -textWidth * 2
+    text.style.marginLeft = textPos + 'px'
 }
 
 document.getElementById('color4').onclick = function() {
-    pos = -width * 3
-    list.style.marginLeft = pos + 'px'
+    imgPos = -imgWidth * 3
+    img.style.marginLeft = imgPos + 'px'
+
+    textPos = -textWidth * 3
+    text.style.marginLeft = textPos + 'px'
 }
